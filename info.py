bdc070357134f7a27cd8d87c02c58772a00d6a15
vinaychauhan01 <147112817+vinaychauhan01@users.noreply.github.com>
2024-01-19 09:15:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -46,8 +46,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://1:1@cluster0.urtr7ne.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Vinayjaat698:vinaych12345@grininja.idbqdtu.mongodb.net/?retryWrites=true")
+DATABASE_NAME = environ.get('DATABASE_NAME', "grininja")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 #Clone
